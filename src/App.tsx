b0a2bf5c3eba0a2a090b9b07f3fab36cda0b9c62
Connor Claxton <czclaxton@gmail.com>
2020-03-13 15:10:14 -0500

    basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { TodoListItem } from "./TodoListItem";
 
-function App() {
+const todos: Array<Todo> = [
+  { text: "Walk the dog", complete: false },
+  { text: "Practice Typescript", complete: true },
+  { text: "Do the dishes", complete: false }
+];
+
+const App = () => {
   const renderCell = () => {};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {todos.map((todo, i) => {
+        return <TodoListItem todo={todos[i]} key={i} />;
+      })}
+    </>
   );
-}
+};
 
 export default App;
